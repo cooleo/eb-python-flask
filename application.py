1747d059480d45e5be849aaab80e74722998597a
nguyendanghung <nguyendanghung2008@gmail.com>
2016-05-10 10:55:33 +0700

    update

---
+++ application.py
@@ -0,0 +1,18 @@
+# import flask
+# import os
+ 
+from flask import Flask
+from flask_restful import Resource, Api
+
+application = Flask(__name__)
+api = Api(application)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+ 
+if __name__ == '__main__':
+    application.run(host='0.0.0.0')
